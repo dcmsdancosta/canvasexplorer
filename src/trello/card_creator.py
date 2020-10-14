9fefa10d08207c3e64dd52fdfe14fab552fe254c
Danilo Costa Marim Segura <danilocosta@MacBook-Air-de-Danilo.local>
2020-10-13 22:34:05 -0300

    Implementing due date

--- src/trello/card_creator.py
+++ src/trello/card_creator.py
@@ -1,5 +1,6 @@
 import requests
 import yaml
+from datetime import datetime, timedelta
 
 with open(r'./credentials.yaml') as file:
         parameters = yaml.load(file, Loader=yaml.FullLoader)
@@ -22,14 +23,16 @@ def getLists(id):
 def createCard(issue): 
     url = "https://api.trello.com/1/cards"
 
-    description = """ Olá professor. Por favor, verifique o tópico criado pelo aluno {}\n Link: {}\n Texto: {} """.format(issue['user_name'], issue['url'], issue['message'])
-
+    description = """ Olá professor!\n\nPor favor, verifique o tópico criado pelo aluno {}\n\n Link: {}\n\n Texto: {} """.format(issue['user_name'], issue['url'], issue['message'])
+    due_date = datetime.strftime(datetime.now() + timedelta(2), '%Y-%m-%d')
     query = {
         'key': parameters['trello_key'],
         'token': parameters['trello_token'],
         'idList': parameters['trello_list'],
         'name': 'Atividade de fórum',
-        'desc': description
+        'desc': description,
+        'due': due_date
+        
     }
 
     response = requests.request(
